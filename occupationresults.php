039dac449a0731a7bc41dcedf264bbce730a776b
davidm03 <davidmcdowall03@gmail.com>
2019-11-16 03:36:32 +0000

    occupation line graph complete

--- occupationresults.php
+++ occupationresults.php
@@ -43,40 +43,80 @@ $(document).ready(function() {
             $('<h1/>',{text: result.title}).appendTo('#content');
             $('<p/>',{text: result.description}).appendTo('#content');
             $('<h4/>',{text: 'Occupational Statistics', style: 'text-align: center'}).appendTo('#content');
-            $('<div/>',{id: 'chartContainer', style: 'width: 50%;'}).appendTo('#content');
+            $('<div/>',{id: 'barGraph', style: 'width: 50%;'}).appendTo('#content');
+            $('<div/>',{id: 'lineGraph', style: 'width: 50%; float: right;'}).appendTo('#content');
          
 
-            var request = "http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=" + result.soc;
-            var req = $.ajax({
-                url: request,
+            var estPayApi = "http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=" + result.soc;
+            var estPayReq = $.ajax({
+                url: estPayApi,
                 dataType: "jsonp"
             });
-            req.done(function(data){
+            estPayReq.done(function(data){
                 if (data.series && data.series.length > 0) {
                     console.log(data);
 
-                    var dps = [];
+                    var payData = [];
                     for(var element in data.series) {
-                        dps.push({label: data.series[element].year, y: data.series[element].estpay});
+                        payData.push({label: data.series[element].year, y: data.series[element].estpay});
                     }
 
-                    var chart = new CanvasJS.Chart("chartContainer",
+                    var chart = new CanvasJS.Chart("barGraph",
                     {   title: {text: 'Rate of Pay (Per Week) Comparison'},     
                         data: [
                         {
                             type: "column",
-                            dataPoints: dps
+                            dataPoints: payData
                         }					
                         ]
                     });
 
                     chart.render();
                 }
-            });      
+            });
 
+            //line
+            var changeApi = "http://api.lmiforall.org.uk/api/v1/ashe/annualChanges?soc=" + result.soc;
+            var changeReq = $.ajax({
+                url: changeApi,
+                dataType: "jsonp"
+            });
+            changeReq.done(function(data){
+                if (data.annual_changes && data.annual_changes.length > 0) {
+                    console.log(data);
 
+                    var changeData = [];
+                    for(var element in data.annual_changes) {
+                        changeData.push({x: data.annual_changes[element].year, y: data.annual_changes[element].change});
+                    }
 
-            
+                    var chart2 = new CanvasJS.Chart("lineGraph", {
+
+                    title:{
+                        text: "Annual Pay Rate Change"
+                    },
+                    axisX:{
+                        valueFormatString: "#"
+                    },
+                    axisY:{ 
+                        title: "Change",
+                        includeZero: false, 
+                        interval: .5,
+                        suffix: "%",
+                        valueFormatString: "#.0"
+                    },
+                    data: [{
+                        type: "line",
+                        yValueFormatString: "#0.0"%"",
+                        xValueFormatString: "#",
+                        markerSize: 5,
+                        dataPoints: changeData
+                    }]
+                });
+                    chart2.render();
+                }
+            });//line end
+
             
         }
 
