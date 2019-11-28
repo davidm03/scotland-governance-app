8a1e9b1cdd5b486494e3db2dfd2385329cc13e55
davidm03 <davidmcdowall03@gmail.com>
2019-11-28 00:40:01 +0000

    Loading JS animation implemented on result pages.

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.min.js"></script>
   <script src="https://canvasjs.com/assets/script/jquery.canvasjs.min.js"></script>
   
--- search_results.php
+++ search_results.php
@@ -3,6 +3,22 @@ include("includes/header.php");
 $search_input = $_POST['postcodeInput'];
 ?>
 
+<style>
+.no-js #loader { display: none;  }
+.js #loader { display: block; position: absolute; left: 100px; top: 0; }
+.pre-load {
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	height: 100%;
+	z-index: 9999;
+	background: url(images/loading.gif) center no-repeat #fff;
+}
+</style>
+
+<div class="pre-load"></div>
+
 <article class="content" id="content">
     <ul class="breadcrumb">
         <li><a href="index.php">Home</a></li>
@@ -187,7 +203,7 @@ $(document).ready(function() {
                 }]
             });
             chart.render();
-                
+            $(".pre-load").fadeOut("slow");                
             });            
         }
 
