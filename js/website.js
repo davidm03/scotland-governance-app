8f433331ad397abe65b340e45a68caf05d1b9a91
davidm03 <davidmcdowall03@gmail.com>
2019-12-02 22:22:51 +0000

    JS clean-up

--- js/website.js
+++ js/website.js
@@ -9,6 +9,47 @@ function noResultsFound() {
 }
 
 //BEGIN FUNCTIONS FOR SEARCH_RESULTS.PHP - GETTING AREA BREAKDOWN INFORMATION
+function lookupPostcode(searchInput, mapInput) {
+    if (searchInput != "") {
+        //if user input is not a valid postcode
+        if (isValidPostcode(searchInput) == false) {
+            //display no results found and throw javascript error
+            noResultsFound();
+            throw new Error('This is not an error. This is just to abort javascript');
+        }
+
+        // if the input string contains a blank space 
+        if (searchInput != null && searchInput.indexOf(' ') >= 0) {
+            // encode the URI component to replace the blank space with '%20' 
+            searchInput = encodeURIComponent(searchInput.trim())
+        }
+
+        var requestURL = "https://api.postcodes.io/scotland/postcodes/" + searchInput;
+
+        console.log(requestURL);
+
+        var req = $.ajax({
+            url: requestURL,
+            dataType: "json"
+        });
+        req.done(function (data) {
+            console.log(data);
+            if (data.status == 200) {
+                var constit_code = data.result.codes.scottish_parliamentary_constituency;
+                console.log(constit_code);
+                getConstituency(constit_code);
+            }
+            else {
+                noResultsFound();
+            }
+        });
+    }
+    else {
+        getConstituency(mapInput);
+    }
+}
+
+
 //GET CONSTITUENCY FROM API
 function getConstituency(constit_code) {
     var requestURL = "https://data.parliament.scot/api/constituencies";
@@ -284,3 +325,118 @@ function getVacancies(postcode, job) {
     }
 }
 //END FUNCTIONS FOR VACANCIES_RESULTS.PHP
+
+//BEGIN FUNCTIONS FOR OCCUPATIONS_RESULTS.PHP
+//FUNCTION TO LOOKUP OCCUPATIONS FROM API
+function lookupOccupation(input) {
+    var requestURL = "http://api.lmiforall.org.uk/api/v1/soc/search?q=" + input;
+
+    console.log(requestURL);
+
+    var req = $.ajax({
+        url: requestURL,
+        dataType: "jsonp"
+    });
+    req.done(function (data) {
+        if (data && data.length > 0) {
+            var result = data[0];
+
+            populatePage(result);
+        }
+        else {
+            noResultsFound();
+        }
+    });
+}
+
+//FUNCTION TO POPULATE OCCUPATIONS_RESULTS PAGE WITH DATA
+function populatePage(result) {
+    $('<h1/>', { text: result.title }).appendTo('#content');
+    $('<p/>', { text: result.description }).appendTo('#content');
+    $('<h4/>', { text: 'Occupational Statistics', style: 'text-align: center' }).appendTo('#content');
+    $('<div/>', { id: 'chartContainer', class: 'wrapper' }).appendTo('#content');
+    $('<div/>', { id: 'one' }).appendTo('#chartContainer');
+    $('<div/>', { id: 'two' }).appendTo('#chartContainer');
+
+    drawEstimatedPayGraph();
+    drawAnnualChangeGraph();
+
+    //FUNCTION TO DRAW THE ESTIMATED PAY GRAPH TO SCREEN
+    function drawEstimatedPayGraph() {
+        var estPayApi = "http://api.lmiforall.org.uk/api/v1/ashe/estimatePay?soc=" + result.soc;
+        var estPayReq = $.ajax({
+            url: estPayApi,
+            dataType: "jsonp"
+        });
+        estPayReq.done(function (data) {
+            if (data.series && data.series.length > 0) {
+                console.log(data);
+
+                var payData = [];
+                for (var element in data.series) {
+                    payData.push({ label: data.series[element].year, y: data.series[element].estpay });
+                }
+
+                var chart = new CanvasJS.Chart("one",
+                    {
+                        title: { text: 'Rate of Pay (Per Week) Comparison', fontFamily: 'Roboto' },
+                        data: [
+                            {
+                                type: "column",
+                                dataPoints: payData
+                            }
+                        ]
+                    });
+
+                chart.render();
+            }
+        });
+    }
+
+    //FUNCTION TO DRAW ANNUAL CHANGE GRAPH TO SCREEN
+    function drawAnnualChangeGraph() {
+        var changeApi = "http://api.lmiforall.org.uk/api/v1/ashe/annualChanges?soc=" + result.soc;
+        var changeReq = $.ajax({
+            url: changeApi,
+            dataType: "jsonp"
+        });
+        changeReq.done(function (data) {
+            if (data.annual_changes && data.annual_changes.length > 0) {
+                console.log(data);
+
+                var changeData = [];
+                for (var element in data.annual_changes) {
+                    changeData.push({ x: data.annual_changes[element].year, y: data.annual_changes[element].change });
+                }
+
+                var chart2 = new CanvasJS.Chart("two", {
+
+                    title: {
+                        text: "Annual Pay Rate Change",
+                        fontFamily: 'Roboto'
+                    },
+                    axisX: {
+                        valueFormatString: "#"
+                    },
+                    axisY: {
+                        title: "Change",
+                        includeZero: false,
+                        interval: 5,
+                        suffix: "%",
+                        valueFormatString: "#.0"
+                    },
+                    data: [{
+                        type: "line",
+                        yValueFormatString: "#0.0" % "",
+                        xValueFormatString: "#",
+                        markerSize: 5,
+                        dataPoints: changeData
+                    }]
+                });
+                chart2.render();
+                $(".pre-load").fadeOut("slow");
+            }
+        });
+    }
+}
+//END FUNCTIONS FOR OCCUPATIONS_RESULTS.PHP
--- search_results.php
+++ search_results.php
@@ -37,48 +37,12 @@ else{
 
 <script>
 $(document).ready(function() {
-        var searchInput = '<?php echo $search_input ?>';        
+        var searchInput = '<?php echo $search_input ?>';
+        var mapInput = '<?php echo $map_input?>';        
         var ward;
         var mspID;           
 
-            if(searchInput!=""){
-                //if user input is not a valid postcode
-                if(isValidPostcode(searchInput)==false){
-                    //display no results found and throw javascript error
-                    noResultsFound();
-                    throw new Error('This is not an error. This is just to abort javascript');
-                }
-
-                // if the input string contains a blank space 
-                if(searchInput!=null && searchInput.indexOf(' ')>=0) {
-                    // encode the URI component to replace the blank space with '%20' 
-                    searchInput = encodeURIComponent(searchInput.trim())
-                }
-
-                var requestURL = "https://api.postcodes.io/scotland/postcodes/" + searchInput;           
-
-                console.log(requestURL);
-
-                var req = $.ajax({
-                url: requestURL,
-                dataType: "json"
-                });
-                req.done(function(data){
-                    console.log(data);
-                    if(data.status==200){
-                        var constit_code = data.result.codes.scottish_parliamentary_constituency;
-                        console.log(constit_code);
-                        getConstituency(constit_code);  
-                    }
-                    else{
-                        noResultsFound();
-                    }                
-                });  
-            }
-            else{
-                var mapInput = '<?php echo $map_input?>';
-                getConstituency(mapInput);
-            }
+        lookupPostcode(searchInput, mapInput);
 });
 </script>
 
